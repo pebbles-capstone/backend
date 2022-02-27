e09b9eda4c91bd74c5e408c5d17dba72868cb074
EC2 Default User <ec2-user@ip-172-31-86-172.ec2.internal>
2022-02-27 01:43:34 +0000

    Implemented Project Service api call

--- API/dist/projectService.js
+++ API/dist/projectService.js
@@ -1,13 +1,28 @@
 "use strict";
 
-module.exports.getProjects = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "getProjects executed successfully!",
-      input: event,
-    }),
-  };
+const AWS = require('aws-sdk'); //eslint-disable-line import/no-extraneous-dependencies
 
-  callback(null, response);
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: ProjectTable,
+};
+
+module.exports.getProjects = (event, context, callback) => {
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        body: 'No Projects found',
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+
+    callback(null, response);
+  });
 };
