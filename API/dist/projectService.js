c7bf07a6b63b7c8db29784003b5f3cc5c35ae6a1
Krishna Solanki <krishna.solanki@mail.utoronto.ca>
2022-02-28 20:32:57 -0500

    Updated getProj endpoint to support CORS

--- API/dist/projectService.js
+++ API/dist/projectService.js
@@ -20,6 +20,10 @@ module.exports.getProjects = (event, context, callback) => {
 
     const response = {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
       body: JSON.stringify(result.Items),
     };
 
