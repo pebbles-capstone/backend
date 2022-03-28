b4aeb4bb839ba38e035f4c7bfca7b3c102a47244
Krishna Solanki <krishna.solanki@mail.utoronto.ca>
2022-03-28 11:53:02 -0400

    Bumped up node version to use optional chaining, added response helper, added try/catchand improved logging

--- API/dist/projectService.js
+++ API/dist/projectService.js
@@ -7,26 +7,43 @@ const params = {
   TableName: "ProjectTable",
 };
 
+// response helper
+const response = (statusCode, body, additionalHeaders) => ({
+  statusCode,
+  body: JSON.stringify(body),
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    ...additionalHeaders,
+  },
+});
+
 module.exports.getProjects = (event, context, callback) => {
-  dynamoDb.scan(params, (error, result) => {
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        body: "No Projects found",
-      });
-      return;
-    }
+  try {
+    dynamoDb.scan(params, (error, result) => {
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          body: "No Projects found",
+        });
+        return;
+      }
 
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-      },
-      body: JSON.stringify(result.Items),
-    };
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(result.Items),
+      };
 
-    callback(null, response);
-  });
+      callback(null, response);
+    });
+  } catch (err) {
+    console.error("Failed: " + err.message);
+    console.error("Trace: " + err.stack);
+    return response(400, { message: err.message });
+  }
 };
--- API/dist/recService.js
+++ API/dist/recService.js
@@ -1,13 +1,52 @@
 "use strict";
 
-module.exports.getRecs = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "getRecs executed successfully!",
-      input: event,
-    }),
-  };
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
+const uuid = require("uuid");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  callback(null, response);
+// response helper
+const response = (statusCode, body, additionalHeaders) => ({
+  statusCode,
+  body: JSON.stringify(body),
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    ...additionalHeaders,
+  },
+});
+module.exports.getRecs = (event, context, callback) => {
+  try {
+    const params = {
+      TableName: "RecsTable",
+      Key: {
+        userId: event.pathParameters.id,
+      },
+    };
+
+    dynamoDb.getItem(params, (error, result) => {
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          body: "No User found",
+        });
+        return;
+      }
+
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(result.Items),
+      };
+
+      callback(null, response);
+    });
+  } catch (err) {
+    console.error("Failed: " + err.message);
+    console.error("Trace: " + err.stack);
+    return response(400, { message: err.message });
+  }
 };
