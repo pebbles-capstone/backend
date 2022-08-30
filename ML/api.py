c3111d8901700a160d960d1deab9161a05c4ee91
Krishna Solanki <39069859+KrishnaSolo@users.noreply.github.com>
2022-08-29 23:44:06 -0700

    Revert "Capstone api dyn model"

---
+++ ML/api.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+from datetime import timedelta
+from re import search
+
+from flask import Flask, Response, request
+from flask_cors import CORS
+
+from Database.config import Config
+from Database.database import db, migrate
+
+app = Flask(__name__)
+app.config.from_object('database.config.Config')
+
+CORS(app, supports_credentials=True)
+
+#Add Routes Here
+
+db.init_app(app)
+migrate.init_app(app, db, directory="database/migrations")
